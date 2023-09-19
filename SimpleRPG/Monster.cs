bf9f32a79485199d9521c6cb2bcffbfca890a33a
Teo Jia Sheng <jiiasheng@hotmail.com>
2023-09-19 15:33:57 +0800

    Initialization

---
+++ SimpleRPG/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRPG
+{
+    public abstract class Monster
+    {
+        private double _health;
+        public double Health
+        {
+            get { return Math.Round(_health, 1); }
+            set { _health = Math.Round(value, 1); }
+        }
+        public string Name { get; set; } = String.Empty;
+        public abstract void PrintOpeningLine();
+        public abstract void PrintDeathScream();
+        public abstract void AttackPlayer(Player player);
+    }
+}
---
+++ SimpleRPG/MonsterFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRPG
+{
+    internal class MonsterFactory
+    {
+        public static Monster? GetMonster(string monsterType)
+        {
+            Monster? monster = null;
+
+            if(monsterType == "Goblin")
+            {
+                monster = new Goblin();
+            }
+            else if(monsterType == "Bandit")
+            {
+                monster = new Bandit();
+            }
+            else if(monsterType == "Dragon")
+            {
+                monster = new Dragon();
+            }
+
+            return monster;
+        }
+    }
+}
